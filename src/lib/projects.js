1f21e43f84eee8ed6fd4c580e98e271ffce3bf7e
Gabriel Cozma <me@gxbs.dev>
2025-07-29 12:24:44 +0300

    feat: redesign homepage

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,13 +1,13 @@
 export const projects = [
   {
     "name": "New School in Sudan",
-    "description": "Building a school in Sudan to provide education to children in need.",
+    "description": "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.",
     "image": "https://assets.globalpartnership.org/s3fs-public/styles/standard_blog_banner/public/slide_ietm/slide_item_image/2016%20GPE_K_Lynch-LU0A7996.jpg?VersionId=XFxz2VJ6b8A1hcxi3hjt_ehDKaqRK4M6&itok=p8GYPAXx",
     "components": [
       {
         "name": "Bricks",
         "needed": 10000,
-        "available": 5025,
+        "available": 5101,
         "price": 0.5,
         "image": "/icons/brick.png",
         "cartCount": 0
@@ -15,7 +15,7 @@ export const projects = [
       {
         "name": "Tables",
         "needed": 50,
-        "available": 41,
+        "available": 50,
         "price": 20,
         "image": "/icons/table.png",
         "cartCount": 0
@@ -23,7 +23,7 @@ export const projects = [
       {
         "name": "Chairs",
         "needed": 100,
-        "available": 85,
+        "available": 100,
         "price": 10,
         "image": "/icons/chair.png",
         "cartCount": 0
@@ -39,7 +39,7 @@ export const projects = [
       {
         "name": "Books",
         "needed": 200,
-        "available": 20,
+        "available": 200,
         "price": 5,
         "image": "/icons/book.png",
         "cartCount": 0
@@ -47,14 +47,29 @@ export const projects = [
     ],
     "leaderboard": [
       {
+        "name": "Happy Owl",
+        "amount": 1134.5,
+        "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=d91abd0a-f0b4-43fc-adf2-ede4424986f8"
+      },
+      {
         "name": "Clever Owl",
         "amount": 227.5,
         "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=7c693338-6881-4483-b520-0cf905fac989"
       },
       {
         "name": "Playful Penguin",
-        "amount": 172.5,
+        "amount": 175,
         "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=61221324-cf5f-4974-bfa4-3db91901d2ec"
+      },
+      {
+        "name": "Happy Dolphin",
+        "amount": 110,
+        "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=96664da9-e6cf-4435-9ed4-466a9189daa9"
+      },
+      {
+        "name": "Happy Fox",
+        "amount": 21,
+        "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=7d5891ca-e9ec-4978-8e05-1204018f9707"
       }
     ]
   },
@@ -66,7 +81,7 @@ export const projects = [
       {
         "name": "Trees",
         "needed": 2000,
-        "available": 4,
+        "available": 39,
         "price": 15,
         "image": "/icons/tree.png",
         "cartCount": 0
@@ -74,9 +89,19 @@ export const projects = [
     ],
     "leaderboard": [
       {
+        "name": "Happy Dolphin",
+        "amount": 360,
+        "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=96664da9-e6cf-4435-9ed4-466a9189daa9"
+      },
+      {
         "name": "Playful Penguin",
         "amount": 180,
         "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=61221324-cf5f-4974-bfa4-3db91901d2ec"
+      },
+      {
+        "name": "Wise Fox",
+        "amount": 165,
+        "profilePicture": "https://api.dicebear.com/6.x/identicon/svg?seed=dc0b8f10-ef22-48d6-ae82-e598374ae086"
       }
     ]
   }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,27 @@
 	import { projects } from '$lib/projects';
 </script>
 
-<div class="flex flex-col gap-5 mt-8">
-	<h2 class="text-3xl font-semibold">Projects</h2>
+<div class="flex flex-col gap-5">
+	<div class="flex flex-col justify-center items-center gap-2">
+		<h2 class="text-3xl font-semibold">Projects</h2>
+		<p class="text-base text-gray-500">Explore our projects and contribute to making a difference.</p>
+	</div>
 	<div class="flex flex-col gap-8 items-center justify-center w-full mt-8">
 		{#each projects as project, index (index)}
 			<div
-				class="flex flex-col justify-center gap-6 w-full text-white rounded-2xl p-18"
+				class="flex flex-col justify-center gap-6 w-full text-white rounded-4xl p-18"
 				style="background: linear-gradient(180deg, rgba(0, 0, 0, 0.30) 79.29%, rgba(0, 0, 0, 0.90) 100%), url({project.image}) lightgray 50% / cover no-repeat;"
 			>
-				<h2 class="text-2xl font-semibold">{project.name}</h2>
+				<h2 class="text-2xl font-bold">{project.name}</h2>
 				<p class="max-w-72">{project.description}</p>
-				<div class="flex flex-row gap-2">
+				<div class="flex flex-row flex-wrap gap-2">
 					<a
 						href="/projects/{index}"
-						class="flex items-center justify-center py-1 rounded-full px-8 bg-white text-black text-lg font-semibold"
+						class="flex items-center justify-center rounded-full px-8 bg-white/70 hover:bg-white/90 transition-colors duration-400 border border-white/70 backdrop-blur-xs text-black text-base font-semibold"
 						>Donate</a
 					>
 					<button
-						class="flex items-center justify-center py-1 rounded-full px-8 border border-white text-white text-lg font-semibold cursor-pointer"
+					class="flex items-center justify-center rounded-full px-8 py-1 bg-transparent hover:bg-white/10 border border-white/30 backdrop-blur-xs text-white text-base font-semibold transition-colors duration-500 cursor-pointer"
 						on:click={() => navigator.clipboard.writeText(`/projects/${index}`)}
 					>
 						Share
