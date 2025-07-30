34e251f5bd754099a62a8ff32c4964fcb05656ed
Gabriel Cozma <me@gxbs.dev>
2025-07-30 23:09:31 +0300

    feat: add org array

---
+++ src/lib/organizations.js
@@ -0,0 +1,26 @@
+export const organizations = [
+	{
+		name: 'Global Education Fund',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+		logo: 'https://placehold.co/150x50?text=GEF'
+	},
+	{
+		name: 'Education for All',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Ali Pfam in hendrerit urna. Pellentesque sit amet sapien.',
+		logo: 'https://placehold.co/150x50?text=EFA'
+	},
+	{
+    name: 'Future Leaders Initiative',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+    logo: 'https://placehold.co/150x50?text=FLI'
+  },
+  {
+    name: 'Youth Empowerment Network',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+    logo: 'https://placehold.co/150x50?text=YEN'
+  }
+];
